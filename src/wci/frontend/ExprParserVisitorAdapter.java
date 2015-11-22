2d6e76a454109341cec7e92e54354604faa1027f
aish27 <aishwaryavnkt@yahoo.com>
2015-11-21 17:16:00 -0800

    Fixed a bug in the code that prevented javacc compilation

--- src/wci/frontend/ExprParserVisitorAdapter.java
+++ src/wci/frontend/ExprParserVisitorAdapter.java
@@ -228,4 +228,16 @@ public class ExprParserVisitorAdapter implements ExprParserVisitor
       return node.childrenAccept(this, data);    
     }
 
+    @Override
+    public Object visit(ASTFor node, Object data)
+    {
+       return node.childrenAccept(this, data);    
+    }
+
+    @Override
+    public Object visit(ASTWhile node, Object data)
+    {
+        return node.childrenAccept(this, data);    
+    }
+
 }
